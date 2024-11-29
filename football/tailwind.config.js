f35f2fbea3eb939a6871f9ad651f418bdfc5763e
AnirudhGirish <anirudhgirish08@gmail.com>
2024-11-29 22:20:53 +0530

    Designed a new website added favicon, text-clip rgb on hover

---
+++ football/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'foot-dark':'#003554',
+        'foot-mid':'#006494',
+        'foot-light':'#0582ca',
+      },
+    },
+  },
+  plugins: [],
+}
